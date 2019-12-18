ba550b330f15ed5538f485ef0f737dc87f2c9e70
Yang Min Chao <joe831204@gmail.com>
2019-12-18 23:37:44 +0800

    update

--- app.py
+++ app.py
@@ -37,16 +37,6 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    r = "我看不懂你說什麼"
-
-    if msg in ["hi", "Hi"]:
-        r = "hi"
-    if msg == "你吃飯了嗎":
-        r = "還沒"
-    elif msg == "你是誰"
-        r = "機器人"
-    
 
     line_bot_api.reply_message(
         event.reply_token,
